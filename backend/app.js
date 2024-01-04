cbc634a693186e339eb14b90a4a8b7641b8bb0f5
Gabriel Aloho <alohoaondoaver@gmail.com>
2024-01-04 18:26:57 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');  // Import Passport
 const config = require('./config');
 const workersRoutes = require('./routes/workersRoutes');
+const passportStrategy = require('./passportStrategy');  // Import your Passport strategy
 
 const app = express();
 
@@ -15,6 +17,12 @@ config.connectToMongoDB()
   .then((mongoClient) => {
     // Specify the database to use
     const db = mongoClient.db('workerdb');
+
+    // Configure Passport
+    passportStrategy(passport);  // Pass Passport to your strategy configuration function
+
+    // Passport middleware
+    app.use(passport.initialize());
 
     // Routes
     app.use('/api/workers', workersRoutes);
