59603865d1b5375136b620b4ee2ec08244e114df
Gabriel Aloho <alohoaondoaver@gmail.com>
2024-01-04 18:17:21 +0100

    Update passportStrategy.js

--- backend/passportStrategy.js
+++ backend/passportStrategy.js
@@ -1,23 +1,26 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const config = require('./config');
-const User = require('./models/userModel');
+const Worker = require('./models/workerModel');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secret;
 
 module.exports = (passport) => {
-  const opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-  opts.secretOrKey = config.secret;
+  passport.use(
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+      try {
+        const worker = await Worker.findById(jwt_payload.id);
 
-  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    User.findById(jwt_payload.id, (err, user) => {
-      if (err) {
-        return done(err, false);
+        if (worker) {
+          return done(null, worker);
+        } else {
+          return done(null, false);
+        }
+      } catch (error) {
+        return done(error, false);
       }
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    });
-  }));
+    })
+  );
 };
