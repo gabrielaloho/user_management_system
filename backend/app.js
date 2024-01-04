639e7507d7e692df65993a12783771484d7169d6
Gabriel Aloho <alohoaondoaver@gmail.com>
2024-01-04 17:11:57 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,31 @@
+// app.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const passport = require('passport');
 const config = require('./config');
+const workersRoutes = require('./routes/workersRoutes');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(passport.initialize());
-require('./passportStrategy')(passport);
 
 // Connect to MongoDB
-mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-mongoose.connection.on('error', (err) => {
-  console.error(`MongoDB connection error: ${err}`);
-  throw new Error(`MongoDB connection error: ${err}`);
-});
+config.connectToMongoDB()
+  .then((mongoClient) => {
+    // Specify the database to use
+    const db = mongoClient.db('workerdb');
 
-// Routes
-const workersRoutes = require('./routes/workersRoutes');
-app.use('/api/workers', workersRoutes);
+    // Routes
+    app.use('/api/workers', workersRoutes);
 
-// Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    // Server
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB:", error);
+    process.exit(1); // Exit the process on connection failure
+  });
